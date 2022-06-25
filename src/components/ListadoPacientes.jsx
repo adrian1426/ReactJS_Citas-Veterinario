e3e2427a4fa1805ddb7d4419a72ee84c7b86ae12
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-06-25 14:56:24 -0500

    Listado de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,17 @@
+import ListadoPacientesItem from "./ListadoPacientesItem";
+
 const ListadoPacientes = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
-      <h2>Lista de pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-2xl text-center">Lista de pacientes</h2>
+      <p className="text-lg text-center mt-2 mb-5">
+        Administra tus <span className="text-indigo-600 font-bold"> pacientes y citas</span>
+      </p>
+
+      <ListadoPacientesItem />
+      <ListadoPacientesItem />
+      <ListadoPacientesItem />
+      <ListadoPacientesItem />
     </div>
   );
 };
---
+++ src/components/ListadoPacientesItem.jsx
@@ -0,0 +1,37 @@
+const ListadoPacientesItem = () => {
+  return (
+    <div className="mt-4 ml-2 px-4 py-4 bg-white shadow-md rounded-md">
+      <p
+        className="font-bold text-gray-700 uppercase mb-2"
+      >
+        Nombre: <span className="font-normal normal-case">Hok</span>
+      </p>
+
+      <p
+        className="font-bold text-gray-700 uppercase mb-2"
+      >
+        Propietario: <span className="font-normal normal-case">Hok</span>
+      </p>
+
+      <p
+        className="font-bold text-gray-700 uppercase mb-2"
+      >
+        Email: <span className="font-normal normal-case">Hok</span>
+      </p>
+
+      <p
+        className="font-bold text-gray-700 uppercase mb-2"
+      >
+        Fecha Alta: <span className="font-normal normal-case">Hok</span>
+      </p>
+
+      <p
+        className="font-bold text-gray-700 uppercase mb-2"
+      >
+        Sintomas: <span className="font-normal normal-case">Hok</span>
+      </p>
+    </div>
+  );
+};
+
+export default ListadoPacientesItem;
