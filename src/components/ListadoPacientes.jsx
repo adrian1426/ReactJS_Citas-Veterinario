81a7fc4511520251c76fff14d4cd05774afd5c58
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-07-02 16:23:33 -0500

    texto condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,11 +3,14 @@ import ListadoPacientesItem from "./ListadoPacientesItem";
 const ListadoPacientes = (props) => {
   const { pacientes } = props;
 
+  const tituloListaPacientes = pacientes.length === 0 ? 'No hay pacientes' : 'Lista de pacientes';
+  const textoListaPacientes = pacientes.length === 0 ? 'Debes agregar tus' : 'Administra tus';
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-2xl text-center">Lista de pacientes</h2>
+      <h2 className="font-black text-2xl text-center">{tituloListaPacientes}</h2>
       <p className="text-lg text-center mt-2 mb-5">
-        Administra tus <span className="text-indigo-600 font-bold"> pacientes y citas</span>
+        {textoListaPacientes} <span className="text-indigo-600 font-bold"> pacientes y citas</span>
       </p>
 
       {pacientes.map((paciente, i) => {
