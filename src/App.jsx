dc60592902a00702319a79fb2435ce784e602b4f
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-07-02 20:11:02 -0500

    localStorage datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Header from './components/Header';
 import ListadoPacientes from './components/ListadoPacientes';
@@ -6,6 +6,15 @@ import ListadoPacientes from './components/ListadoPacientes';
 function App() {
   const [pacientes, setPacientes] = useState([]);
   const [pacienteEdit, setPacienteEdit] = useState({});
+
+  useEffect(() => {
+    const pacientesLocal = JSON.parse(localStorage.getItem('pacientes'));
+    pacientesLocal?.length > 0 && setPacientes(pacientesLocal);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
 
   return (
     <div className='container mx-auto mt-5'>
